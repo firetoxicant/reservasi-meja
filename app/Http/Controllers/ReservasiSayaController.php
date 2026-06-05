b49033c86bcc6e7211c110a975aa393814b7be67
kukuh wisanggeni <kukuhwisanggeni09@gmail.com>
2026-06-05 17:40:50 +0700

    buat fungsi PesananSaya

--- app/Http/Controllers/ReservasiSayaController.php
+++ app/Http/Controllers/ReservasiSayaController.php
@@ -12,54 +12,17 @@ class ReservasiSayaController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $id_user = auth()->id();
+        $tanggal_sekarang = date('Y-m-d');
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+        // Mengambil data reservasi milik user login yang tanggalnya hari ini ke atas
+        // Menggunakan Eager Loading untuk mengambil data relasi meja, pembayaran, dan detail order menu sekaligus
+        $reservasis = Reservasi::with(['meja', 'pembayaran', 'orders.menu'])
+            ->where('id_pelanggan', $id_user)
+            ->where('tanggal_reservasi', '>=', $tanggal_sekarang)
+            ->orderBy('id', 'desc')
+            ->get();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return view('reservasi.saya', compact('reservasis'));
     }
 }
